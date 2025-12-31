0af738c8cc7306f8f12daa148f06ef1c633957ac
Matthew Quinn <iam@matthewquinn.me>
2025-12-31 13:35:45 +1300

    Fixes extension typing

--- lib/types.ts
+++ lib/types.ts
@@ -11,16 +11,37 @@ export interface StaticContext {
 }
 
 export interface Extension {
+
+  /**
+   * The name of the extension.
+   */
   name: string;
+
+  /**
+   * Setup method which can perform async setup tasks
+   * and report status via a readable stream.
+   */
   setup?(): ReadableStream;
-  getAsset?(): StaticAsset | undefined;
+
+  /**
+   * Retrieves a static assets from the extension.
+   *
+   * @param assetAlias The alias for the asset.
+   */
+  getAsset?(assetAlias: string): StaticAsset | undefined;
+
+  /**
+   * Root level aliases the extension uses to identify
+   * the assets it manages.
+   */
   staticAliases?: string[];
+
 };
 
 export interface StaticExtension {
 
   /**
-   * The name of the static extension.
+   * The name of the extension.
    */
   name: string;
 
